607ed31d0f1dfa72eb4d1f0dd8e6b5b52ff4c9c8
Yman <ymanneil07@gmail.com>
2024-10-12 16:24:06 +0800

    enemy ai
    nav mesh navigation package and script

---
+++ Seeker/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    UnityEngine.AI.NavMeshAgent navMeshAgent;
+
+    
+    void Start()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
